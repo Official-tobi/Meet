5e18c17dd8f0ef696febdc7a6073ed6d12ba8ce1
Tobi Ademola <meademolatobi@gmail.com>
2024-03-17 21:19:50 +0100

    Implement hours array filter & render free hours
    Implemented hours array filter and rendered the free hours (if the fetched data is an unpopulated array, the application renders all hours to the screen but if the fetched data is a populated array, the application renders only the hours in the array and the intervals between them as the available hours for scheduling meetings)
    
    Still unable to fetch for the current day, still unable to update the backend with new events, and still unable to interpret all back end responses

--- src/App.tsx
+++ src/App.tsx
@@ -6,51 +6,84 @@ import { Button } from "./components/ui/button";
 import { Toaster } from "./components/ui/sonner";
 import { toast } from "sonner";
 
+// Type for payload data
 interface Payload {
   start: string;
   end: string;
   showAs: string;
   isBusyAllDay: boolean;
 }
+// This array contains all the available working hours in the right order from 7AM to 6PM
+// The hours array must always contain the hours in ascending order
+const hours: string[] = [
+  "06:00:00",
+  "06:30:00",
+  "07:00:00",
+  "07:30:00",
+  "08:00:00",
+  "08:30:00",
+  "09:00:00",
+  "09:30:00",
+  "10:00:00",
+  "10:30:00",
+  "11:00:00",
+  "11:30:00",
+  "12:00:00",
+  "12:30:00",
+  "13:00:00",
+  "13:30:00",
+  "14:00:00",
+  "14:30:00",
+  "15:00:00",
+  "15:30:00",
+  "16:00:00",
+  "16:30:00",
+  "17:00:00",
+  "17:30:00",
+  "18:00:00",
+];
+const filter = (timeInt: string[], startTime: string, endTime: string) => {
+  const startTimeIndex = timeInt.indexOf(startTime);
+  const endTimeIndex = timeInt.indexOf(endTime);
 
+  if (startTimeIndex == -1 || endTimeIndex == -1) {
+    return [];
+  }
+
+  const start = Math.min(startTimeIndex, endTimeIndex);
+  const end = Math.max(startTimeIndex, endTimeIndex);
+
+  return timeInt.slice(start, end + 1);
+};
 function App() {
+  // Date state passed into the calendar component and is mutated by the calendar component
   const [date, setDate] = useState<Date | undefined>(new Date());
+  // Data state the holds the payload from the fetch statement
   const [data, setData] = useState<Payload[]>([]);
-  const hours: string[] = [
-    "6:00:00",
-    "6:30:00",
-    "7:00:00",
-    "7:30:00",
-    "8:00:00",
-    "8:30:00",
-    "9:00:00",
-    "9:30:00",
-    "10:00:00",
-    "10:30:00",
-    "11:00:00",
-    "11:30:00",
-    "12:00:00",
-    "12:30:00",
-    "13:00:00",
-    "13:30:00",
-    "14:00:00",
-    "14:30:00",
-    "15:00:00",
-    "15:30:00",
-    "16:00:00",
-    "16:30:00",
-    "17:00:00",
-    "17:30:00",
-    "18:00:00",
-  ];
+
+  // This holds the users available time obtained by filtering the hours state with the data recieved
+  const [freeTimeInt, setFreeTimeInt] = useState<string[]>([]);
+
+  // This prepares the date state to be sent to the backend
+  const formatDateStr = (date: Date) => {
+    return `${date?.getFullYear()}-${
+      date?.getMonth() + 1 < 10
+        ? "0" + (date?.getMonth() + 1)
+        : date.getMonth() + 1
+    }-${date?.getDate()}`;
+  };
+  // This prepares the date & time stat to be sent to the backend
+  // const formatDateTimeStr = (date: Date) => {
+  //   const dateStr = formatDateStr(date);
+  //   const timeStr = date.toString().split(" ")[4];
+  //   return `${dateStr}%20${timeStr}`;
+  // };
+  // Fires a fetch request when the date changes
   useEffect(() => {
     if (date) {
       console.log("fetching schedule...");
-      const dateString = `${date?.getFullYear()}-${
-        date?.getMonth() + 1 < 10
-          ? "0" + (date?.getMonth() + 1)
-          : date.getMonth() + 1
-      }-${date?.getDate()}`;
+
+      const dateString = formatDateStr(date);
 
       const fetchFullday = async (dateStr: string) => {
         try {
@@ -70,11 +103,9 @@ function App() {
           }
 
           const data = await res.json();
-          console.log(data);
-          const message = data.message;
           setData(data.payload);
-          toast(`${message}}`, {
-            description: `The available times have been displayed below`,
+          toast(`${data.message}}`, {
+            description: `The unoccupied times have been displayed below`,
           });
           console.log("fetching done!");
         } catch (error) {
@@ -82,6 +113,7 @@ function App() {
           console.log("fetching done!");
         }
       };
+
       fetchFullday(dateString);
     } else {
       toast("No date selected", {
@@ -93,7 +125,26 @@ function App() {
       });
     }
   }, [date]);
-  console.log(data);
+
+  // This takes in the start and end returned by the backend and formats for usage
+  const formatTime = (time: string) => time.split("T")[1].split(".")[0];
+
+  // This observes the changes in the data state and updates the setFreeTimeInt state with the filtered hours
+  useEffect(() => {
+    //This empties the free hours array each time the data state changes, so that it doesn't hold the value from a previous fetch
+    setFreeTimeInt([]);
+    // This takes in the working hours, start time and end time and returns an array containing the start time the end time and all the values in between
+    if (data.length === 0) {
+      setFreeTimeInt(hours);
+    } else {
+      data.forEach((val) => {
+        setFreeTimeInt((prevVal) => [
+          ...prevVal,
+          ...filter(hours, formatTime(val.start), formatTime(val.end)),
+        ]);
+      });
+    }
+  }, [data]);
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <main className="font-inter w-full grid grid-cols-2 gap-36 h-full items-center font-semibold">
@@ -110,15 +161,30 @@ function App() {
         </section>
         <section className="justify-self-start">
           {date && <p className="w-[30ch] py-4">{date?.toString()}</p>}
+
           {date ? (
             <div className={`grid grid-cols-3 gap-2 w-[30ch]`}>
-              {hours.map((hour, index) => {
-                return (
-                  <Button key={index} variant="secondary">
-                    {hour.substring(0, hour.length - 3)}
-                  </Button>
-                );
-              })}
+              {freeTimeInt ? (
+                freeTimeInt.map((hour, index) => {
+                  return (
+                    <Button key={index} variant="secondary">
+                      {/* this statement cuts of the seconds portion of the time string */}
+                      {hour.substring(0, hour.length - 3)}
+                    </Button>
+                  );
+                })
+              ) : (
+                <div>
+                  {hours.map((hour, index) => {
+                    return (
+                      <Button key={index} variant={`secondary`}>
+                        {/* this statement cuts of the seconds portion of the time string */}
+                        {hour.substring(0, hour.length - 3)}
+                      </Button>
+                    );
+                  })}
+                </div>
+              )}
             </div>
           ) : (
             <p className=" select-none opacity-55">Please select a date</p>
